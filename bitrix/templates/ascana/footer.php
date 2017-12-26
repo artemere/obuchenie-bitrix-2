5013203de0389d71a1749cf116514857c5593a27
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-26 18:04:19 +0300

    Закончил главный шаблон, но он не работает

--- bitrix/templates/ascana/footer.php
+++ bitrix/templates/ascana/footer.php
@@ -86,3 +86,4 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 </body>
 </html>
+
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
-				<section id="banner">
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
+
+    <section id="banner">
 					<header>
 						<h2>Arcana: <em>A responsive site template freebie by <a href="http://html5up.net">HTML5 UP</a></em></h2>
 						<a href="#" class="button">Learn More</a>
