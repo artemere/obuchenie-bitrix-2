faae270f578698b41d80f2d3ce1f5143b676d944
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-27 00:35:07 +0300

    Перед версткой шаблона новосетй

--- novosti/index.php
+++ novosti/index.php
@@ -1,120 +1,169 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новости");
-?><?$APPLICATION->IncludeComponent("bitrix:catalog.section.list", "razrabotka", Array(
-	"COMPONENT_TEMPLATE" => ".default",
-		"IBLOCK_TYPE" => "information",	// Тип инфоблока
-		"IBLOCK_ID" => "12",	// Инфоблок
-		"SECTION_ID" => $_REQUEST["SECTION_ID"],	// ID раздела
-		"SECTION_CODE" => "",	// Код раздела
-		"COUNT_ELEMENTS" => "Y",	// Показывать количество элементов в разделе
-		"TOP_DEPTH" => "2",	// Максимальная отображаемая глубина разделов
-		"SECTION_FIELDS" => array(	// Поля разделов
-			0 => "",
-			1 => "",
-		),
-		"SECTION_USER_FIELDS" => array(	// Свойства разделов
-			0 => "",
-			1 => "",
-		),
-		"VIEW_MODE" => "LIST",	// Вид списка подразделов
-		"SHOW_PARENT_NAME" => "Y",	// Показывать название раздела
-		"SECTION_URL" => "/novosti/#SECTION_CODE_PATH#/",	// URL, ведущий на страницу с содержимым раздела
-		"CACHE_TYPE" => "A",	// Тип кеширования
-		"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
-		"CACHE_GROUPS" => "Y",	// Учитывать права доступа
-		"ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
-	),
-	false
-);?><br>
- <?$APPLICATION->IncludeComponent(
-	"bitrix:news", 
-	".default", 
-	array(
-		"ADD_ELEMENT_CHAIN" => "Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"BROWSER_TITLE" => "-",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "36000000",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"COMPONENT_TEMPLATE" => ".default",
-		"DETAIL_ACTIVE_DATE_FORMAT" => "j F Y",
-		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
-		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"DETAIL_PAGER_SHOW_ALL" => "Y",
-		"DETAIL_PAGER_TEMPLATE" => "",
-		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"DETAIL_SET_CANONICAL_URL" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "12",
-		"IBLOCK_TYPE" => "information",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"LIST_ACTIVE_DATE_FORMAT" => "j F Y",
-		"LIST_FIELD_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"LIST_PROPERTY_CODE" => array(
-			0 => "",
-			1 => "",
-		),
-		"MESSAGE_404" => "",
-		"META_DESCRIPTION" => "-",
-		"META_KEYWORDS" => "-",
-		"NEWS_COUNT" => "3",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "Y",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"SEF_FOLDER" => "/novosti/",
-		"SEF_MODE" => "Y",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"USE_CATEGORIES" => "N",
-		"USE_FILTER" => "N",
-		"USE_PERMISSIONS" => "N",
-		"USE_RATING" => "N",
-		"USE_RSS" => "N",
-		"USE_SEARCH" => "N",
-		"USE_SHARE" => "N",
-		"SEF_URL_TEMPLATES" => array(
-			"news" => "",
-			"section" => "#SECTION_CODE_PATH#/",
-			"detail" => "#SECTION_CODE_PATH#/#ELEMENT_CODE#.html",
-		)
-	),
-	false
-);?><br>
+?>
+
+
+    <section class="wrapper style1">
+        <div class="container">
+            <div class="row 200%">
+                <div class="8u 12u(narrower)">
+                    <div id="content">
+                        <?$APPLICATION->IncludeComponent(
+                            "bitrix:news",
+                            ".default",
+                            array(
+                                "ADD_ELEMENT_CHAIN" => "Y",
+                                "ADD_SECTIONS_CHAIN" => "Y",
+                                "AJAX_MODE" => "N",
+                                "AJAX_OPTION_ADDITIONAL" => "",
+                                "AJAX_OPTION_HISTORY" => "N",
+                                "AJAX_OPTION_JUMP" => "N",
+                                "AJAX_OPTION_STYLE" => "Y",
+                                "BROWSER_TITLE" => "-",
+                                "CACHE_FILTER" => "N",
+                                "CACHE_GROUPS" => "Y",
+                                "CACHE_TIME" => "36000000",
+                                "CACHE_TYPE" => "A",
+                                "CHECK_DATES" => "Y",
+                                "COMPONENT_TEMPLATE" => ".default",
+                                "DETAIL_ACTIVE_DATE_FORMAT" => "j F Y",
+                                "DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+                                "DETAIL_DISPLAY_TOP_PAGER" => "N",
+                                "DETAIL_FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "DETAIL_PAGER_SHOW_ALL" => "Y",
+                                "DETAIL_PAGER_TEMPLATE" => "",
+                                "DETAIL_PAGER_TITLE" => "Страница",
+                                "DETAIL_PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "DETAIL_SET_CANONICAL_URL" => "N",
+                                "DISPLAY_BOTTOM_PAGER" => "Y",
+                                "DISPLAY_DATE" => "Y",
+                                "DISPLAY_NAME" => "Y",
+                                "DISPLAY_PICTURE" => "Y",
+                                "DISPLAY_PREVIEW_TEXT" => "Y",
+                                "DISPLAY_TOP_PAGER" => "N",
+                                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                "IBLOCK_ID" => "12",
+                                "IBLOCK_TYPE" => "information",
+                                "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                                "LIST_ACTIVE_DATE_FORMAT" => "j F Y",
+                                "LIST_FIELD_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "LIST_PROPERTY_CODE" => array(
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "MESSAGE_404" => "",
+                                "META_DESCRIPTION" => "-",
+                                "META_KEYWORDS" => "-",
+                                "NEWS_COUNT" => "3",
+                                "PAGER_BASE_LINK_ENABLE" => "N",
+                                "PAGER_DESC_NUMBERING" => "N",
+                                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                "PAGER_SHOW_ALL" => "Y",
+                                "PAGER_SHOW_ALWAYS" => "N",
+                                "PAGER_TEMPLATE" => ".default",
+                                "PAGER_TITLE" => "Новости",
+                                "PREVIEW_TRUNCATE_LEN" => "",
+                                "SEF_FOLDER" => "/novosti/",
+                                "SEF_MODE" => "Y",
+                                "SET_LAST_MODIFIED" => "N",
+                                "SET_STATUS_404" => "N",
+                                "SET_TITLE" => "Y",
+                                "SHOW_404" => "N",
+                                "SORT_BY1" => "ACTIVE_FROM",
+                                "SORT_BY2" => "SORT",
+                                "SORT_ORDER1" => "DESC",
+                                "SORT_ORDER2" => "ASC",
+                                "STRICT_SECTION_CHECK" => "N",
+                                "USE_CATEGORIES" => "N",
+                                "USE_FILTER" => "N",
+                                "USE_PERMISSIONS" => "N",
+                                "USE_RATING" => "N",
+                                "USE_RSS" => "N",
+                                "USE_SEARCH" => "N",
+                                "USE_SHARE" => "N",
+                                "SEF_URL_TEMPLATES" => array(
+                                    "news" => "",
+                                    "section" => "#SECTION_CODE_PATH#/",
+                                    "detail" => "#SECTION_CODE_PATH#/#ELEMENT_CODE#.html",
+                                )
+                            ),
+                            false
+                        );?>
+                        <!-- Content -->
+                    </div>
+                </div>
+                <div class="4u 12u(narrower)">
+                    <div id="sidebar">
+
+                        <!-- Sidebar -->
+
+                        <section>
+                            <h3>Just a Subheading</h3>
+                            <p>Phasellus quam turpis, feugiat sit amet ornare in, hendrerit in lectus.
+                                Praesent semper mod quis eget mi. Etiam eu ante risus. Aliquam erat volutpat.
+                                Aliquam luctus et mattis lectus sit amet pulvinar. Nam turpis et nisi etiam.</p>
+                            <footer>
+                                <a href="#" class="button">Continue Reading</a>
+                            </footer>
+                        </section>
+
+                        <section>
+                            <h3>Разделы</h3>
+
+                            <?$APPLICATION->IncludeComponent("bitrix:catalog.section.list", "razrabotka", Array(
+                                "COMPONENT_TEMPLATE" => ".default",
+                                "IBLOCK_TYPE" => "information",	// Тип инфоблока
+                                "IBLOCK_ID" => "12",	// Инфоблок
+                                "SECTION_ID" => $_REQUEST["SECTION_ID"],	// ID раздела
+                                "SECTION_CODE" => "",	// Код раздела
+                                "COUNT_ELEMENTS" => "Y",	// Показывать количество элементов в разделе
+                                "TOP_DEPTH" => "2",	// Максимальная отображаемая глубина разделов
+                                "SECTION_FIELDS" => array(	// Поля разделов
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "SECTION_USER_FIELDS" => array(	// Свойства разделов
+                                    0 => "",
+                                    1 => "",
+                                ),
+                                "VIEW_MODE" => "LIST",	// Вид списка подразделов
+                                "SHOW_PARENT_NAME" => "Y",	// Показывать название раздела
+                                "SECTION_URL" => "/novosti/#SECTION_CODE_PATH#/",	// URL, ведущий на страницу с содержимым раздела
+                                "CACHE_TYPE" => "A",	// Тип кеширования
+                                "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+                                "CACHE_GROUPS" => "Y",	// Учитывать права доступа
+                                "ADD_SECTIONS_CHAIN" => "Y",	// Включать раздел в цепочку навигации
+                            ),
+                                false
+                            );?>
+
+
+                            <ul class="links">
+                                <li><a href="#">Amet turpis, feugiat et sit amet</a></li>
+                                <li><a href="#">Ornare in hendrerit in lectus</a></li>
+                                <li><a href="#">Semper mod quis eget mi dolore</a></li>
+                                <li><a href="#">Consequat etiam lorem phasellus</a></li>
+                                <li><a href="#">Amet turpis, feugiat et sit amet</a></li>
+                                <li><a href="#">Semper mod quisturpis nisi</a></li>
+                            </ul>
+                            <footer>
+                                <a href="#" class="button">More Random Links</a>
+                            </footer>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
  <br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
