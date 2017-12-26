2cbea097d2fd55d44b1f6b02a12211fc86252ab8
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-26 16:00:02 +0300

    Initial commit

---
+++ bitrix/managed_cache/MYSQL/agents/10/10624e3811fc831f380b14e8fee934d9.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001514290754';
+$dateexpire = '001514294414';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1514294414;}';
+return true;
+?>
---
+++ bitrix/managed_cache/MYSQL/css/s1/books/kernel_main/info.php
@@ -0,0 +1,1 @@
+<? $arFilesInfo = array( 'FILES' => array("/bitrix/js/main/core/css/core.css" => "15142906832854","/bitrix/js/main/core/css/core_popup.css" => "151429068318079","/bitrix/js/main/core/css/core_date.css" => "15142906839657",), 'CUR_SEL_CNT' => '288', 'CUR_IE_CNT' => '0'); ?>
