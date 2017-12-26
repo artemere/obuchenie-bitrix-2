42bd2766858128e00b1775641b90a39b641d47d8
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-26 17:19:02 +0300

    Правим главный шаблон

--- bitrix/templates/ascana/header.php
+++ bitrix/templates/ascana/header.php
@@ -1,109 +1,144 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<?$APPLICATION->ShowHead()?>
-<title><?$APPLICATION->ShowTitle()?></title>
-</head>
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-<body>
-<div id="panel"><?$APPLICATION->ShowPanel();?></div>
 
-<div id="header"><img src="<?=SITE_TEMPLATE_PATH?>/images/logo.jpg" id="header_logo" height="105" alt="" width="508" border="0"/>
-  <div id="header_text"><?$APPLICATION->IncludeFile(
-			$APPLICATION->GetTemplatePath("include_areas/company_name.php"),
-			Array(),
-			Array("MODE"=>"html")
-		);?> </div>
 
-	<a href="/" title="Главная" id="company_logo"></a>
+$APPLICATION->SetPageProperty("title", "Демонстрационная версия продукта «1С-Битрикс: Управление сайтом»");
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
 
-  <div id="header_menu"><?$APPLICATION->IncludeFile(
-			$APPLICATION->GetTemplatePath("include_areas/header_icons.php"),
-			Array(),
-			Array("MODE"=>"php")
-		);?> </div>
+?> <?$APPLICATION->IncludeComponent(
+    "bitrix:catalog.section.list",
+    "",
+    Array(
+        "IBLOCK_TYPE" => "books",
+        "IBLOCK_ID" => "6",
+        "SECTION_ID" => $_REQUEST["SECTION_ID"],
+        "SECTION_URL" => "/e-store/books/#SECTION_ID#/",
+        "COUNT_ELEMENTS" => "Y",
+        "DISPLAY_PANEL" => "N",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "3600"
+    )
+);?>
+<hr />
 
-</div>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"horizontal_multilevel",
-	Array(
-		"ROOT_MENU_TYPE" => "top", 
-		"MAX_LEVEL" => "3", 
-		"CHILD_MENU_TYPE" => "left", 
-		"USE_EXT" => "Y", 
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"MENU_CACHE_GET_VARS" => Array()
-	)
-);?> 
-<div id="zebra"></div>
-
-<table id="content">
-  <tbody>
-    <tr><td class="left-column"><?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	".default",
-	Array(
-		"ROOT_MENU_TYPE" => "left", 
-		"MAX_LEVEL" => "1", 
-		"CHILD_MENU_TYPE" => "left", 
-		"USE_EXT" => "Y", 
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"MENU_CACHE_GET_VARS" => array(
-			0 => "SECTION_ID",
-			1 => "page",
-		),
-	)
+    "bitrix:catalog.top",
+    "",
+    Array(
+        "IBLOCK_TYPE" => "books",
+        "IBLOCK_ID" => "6",
+        "ELEMENT_SORT_FIELD" => "sort",
+        "ELEMENT_SORT_ORDER" => "asc",
+        "SECTION_URL" => "/e-store/books/#SECTION_ID#/",
+        "DETAIL_URL" => "/e-store/books/#SECTION_ID#/#ELEMENT_ID#/",
+        "BASKET_URL" => "/personal/cart/",
+        "ACTION_VARIABLE" => "action",
+        "PRODUCT_ID_VARIABLE" => "id",
+        "SECTION_ID_VARIABLE" => "SECTION_ID",
+        "DISPLAY_COMPARE" => "N",
+        "ELEMENT_COUNT" => "3",
+        "LINE_ELEMENT_COUNT" => "1",
+        "PROPERTY_CODE" => Array(),
+        "PRICE_CODE" => Array("RETAIL"),
+        "USE_PRICE_COUNT" => "N",
+        "SHOW_PRICE_COUNT" => "1",
+        "CACHE_TYPE" => "A",
+        "CACHE_TIME" => "3600"
+    )
 );?>
 
-	<!-- SOCIALNETWORK -->
+<h2>Видео-новости</h2>
 
-        <div class="content-block">
-          <div class="content-block-head">Поиск по сайту</div>
+<?$APPLICATION->IncludeComponent(
+    "bitrix:player",
+    "",
+    Array(
+        "PLAYER_TYPE" => "auto",
+        "USE_PLAYLIST" => "N",
+        "PATH" => "/upload/intro.flv",
+        "WIDTH" => "400",
+        "HEIGHT" => "324",
+        "FULLSCREEN" => "Y",
+        "SKIN_PATH" => "/bitrix/components/bitrix/player/mediaplayer/skins",
+        "SKIN" => "bitrix.swf",
+        "CONTROLBAR" => "bottom",
+        "WMODE" => "transparent",
+        "HIDE_MENU" => "N",
+        "SHOW_CONTROLS" => "Y",
+        "SHOW_STOP" => "N",
+        "SHOW_DIGITS" => "Y",
+        "CONTROLS_BGCOLOR" => "FFFFFF",
+        "CONTROLS_COLOR" => "000000",
+        "CONTROLS_OVER_COLOR" => "000000",
+        "SCREEN_COLOR" => "000000",
+        "WMODE_WMV" => "window",
+        "AUTOSTART" => "N",
+        "REPEAT" => "N",
+        "VOLUME" => "90",
+        "DISPLAY_CLICK" => "play",
+        "MUTE" => "N",
+        "HIGH_QUALITY" => "Y",
+        "ADVANCED_MODE_SETTINGS" => "N",
+        "BUFFER_LENGTH" => "10",
+        "DOWNLOAD_LINK_TARGET" => "_self"
+    ),
+    false
+);?>
 
-          <div class="content-block-body"><?$APPLICATION->IncludeComponent(
-	"bitrix:search.form",
-	".default",
-	Array(
-		"PAGE" => "/search/" 
-	)
-);?> </div>
-        </div>
-      
-        <div class="content-block">
-          <div class="content-block-head">Авторизация</div>
-        
-          <div class="content-block-body"><?$APPLICATION->IncludeComponent(
-	"bitrix:system.auth.form",
-	".default",
-	Array(
-		"REGISTER_URL" => "/auth/", 
-		"PROFILE_URL" => "/personal/profile/" 
-	)
-);?></div>
-        </div>
-      
-		<!--SUBSCRIBE-->
-      
-        <!--BANNER_LEFT-->
+<!-- --><!-- -->
 
-        </div>
-      </td><td class="main-column">
-        <div id="navigation"><?$APPLICATION->IncludeComponent(
-	"bitrix:breadcrumb",
-	".default",
-	Array(
-		"START_FROM" => "0", 
-		"PATH" => "", 
-		"SITE_ID" => "" 
-	)
-);?> </div>
-      
-        <h1 id="pagetitle"><?$APPLICATION->ShowTitle(false)?></h1>
-      
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>$APPLICATION->SetTitle();</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+</head>
+<body>
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <div id="header">
+
+        <!-- Logo -->
+        <h1><a href="index.html" id="logo">Arcana <em>by HTML5 UP</em></a></h1>
+
+        <!-- Nav -->
+        <nav id="nav">
+            <ul>
+                <li class="current"><a href="index.html">Home</a></li>
+                <li>
+                    <a href="#">Dropdown</a>
+                    <ul>
+                        <li><a href="#">Lorem dolor</a></li>
+                        <li><a href="#">Magna phasellus</a></li>
+                        <li><a href="#">Etiam sed tempus</a></li>
+                        <li>
+                            <a href="#">Submenu</a>
+                            <ul>
+                                <li><a href="#">Lorem dolor</a></li>
+                                <li><a href="#">Phasellus magna</a></li>
+                                <li><a href="#">Magna phasellus</a></li>
+                                <li><a href="#">Etiam nisl</a></li>
+                                <li><a href="#">Veroeros feugiat</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Veroeros feugiat</a></li>
+                    </ul>
+                </li>
+                <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                <li><a href="two-sidebar.html">Two Sidebar</a></li>
+                <li><a href="no-sidebar.html">No Sidebar</a></li>
+            </ul>
+        </nav>
+
+    </div>
