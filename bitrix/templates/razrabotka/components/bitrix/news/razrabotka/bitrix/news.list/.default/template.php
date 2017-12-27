9c4563d203db15ad475d2a4f0a601292c6dbc750
Artem Eremenko <artem.eremenko@gmail.com>
2017-12-27 11:20:06 +0300

    шаблон новостей главная ОК

--- bitrix/templates/razrabotka/components/bitrix/news/razrabotka/bitrix/news.list/.default/template.php
+++ bitrix/templates/razrabotka/components/bitrix/news/razrabotka/bitrix/news.list/.default/template.php
@@ -1,16 +1,4 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-/** @var array $arParams */
-/** @var array $arResult */
-/** @global CMain $APPLICATION */
-/** @global CUser $USER */
-/** @global CDatabase $DB */
-/** @var CBitrixComponentTemplate $this */
-/** @var string $templateName */
-/** @var string $templateFile */
-/** @var string $templateFolder */
-/** @var string $componentPath */
-/** @var CBitrixComponent $component */
-$this->setFrameMode(true);
 ?>
 <div class="news-list">
 <?if($arParams["DISPLAY_TOP_PAGER"]):?>
@@ -87,8 +75,10 @@ $this->setFrameMode(true);
 			</small><br />
 		<?endforeach;?>
 	</p>
+
+<*/?>
 <?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
-</div>*/?>
+    <?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
+    <br /><?=$arResult["NAV_STRING"]?>
+    <?endif;?>
+</div>
